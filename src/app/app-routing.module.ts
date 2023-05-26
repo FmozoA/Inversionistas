0ddbe58dd7a98b4706158ac00503466f82bd2394
fmozo <andres.031felipe@hotmail.com>
2023-05-26 12:15:30 -0500

    Arquitectura angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactoInversionistasComponent } from './contacto-inversionistas/contacto-inversionistas.component';
+import { GobiernoCorporativoComponent } from './gobierno-corporativo/gobierno-corporativo.component';
+
+
+
+const routes: Routes = [
+  { path: 'ContactoInversionistas', component: ContactoInversionistasComponent },
+  { path: 'GobiernoCorporativo', component: GobiernoCorporativoComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoInversionistasComponent } from './contacto-inversionistas/contacto-inversionistas.component';
+import { GobiernoCorporativoComponent } from './gobierno-corporativo/gobierno-corporativo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoInversionistasComponent,
+    GobiernoCorporativoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
